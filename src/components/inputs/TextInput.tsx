575f4e2898200a45f1108bdb1022409e901ac943
Daniel Jarrett <daniel.jarrett@jump24.co.uk>
2023-08-18 15:04:43 +0100

    Removing placeholder tailwind class as it does nothing

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -8,7 +8,7 @@ interface Props {
 function TextInput(props: Props) {
   return  (
     <NativeInput
-      className='w-full border-b border-white rounded-md p-2 text-white placeholder:text-white'
+      className='w-full border-b border-white rounded-md p-2 text-white'
       placeholder={props.placeholder}
       placeholderTextColor='#f4f4f5'
     />
