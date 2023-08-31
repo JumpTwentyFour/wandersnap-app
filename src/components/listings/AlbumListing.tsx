31f1d3ee44cb9ae95ed0bd8d7abee2b8b844d9d6
Josh Waite <joshw8994@gmail.com>
2023-08-31 11:17:42 +0100

    Update Album Listing

--- src/components/listings/AlbumListing.tsx
+++ src/components/listings/AlbumListing.tsx
@@ -1,7 +1,6 @@
 import { format } from 'date-fns'
 import React from 'react'
-import { View, Text, Image } from 'react-native'
-// import { Image } from 'expo-image'
+import { View, Text, Image, ImageBackground } from 'react-native'
 
 interface AlbumListingProps {
   title: string
@@ -18,29 +17,38 @@ function AlbumListing(props: AlbumListingProps) {
   } = props
 
   return (
-    <View className="flex flex-col items-start self-stretch gap-4 pl-1">
-      <View className="flex flex-col items-start self-stretch gap-1">
+    <View className="flex flex-col items-start self-stretch p-2">
+      <View className="flex flex-col items-start self-stretch">
         <Text className="font-comfortaa text-[22px] text-white w-full">
           {title}
         </Text>
-        <Text className="w-full text-xs text-white font-mons">
+        <Text className="w-full mt-1 text-xs text-white font-mons">
           {format(dateFrom, 'yyyy MM dd')} - {format(dateTo, 'yyyy MM dd')}
         </Text>
       </View>
-      <View className="flex flex-row items-start justify-start w-full h-auto">
-        <Image
-          source={require('~/public/images/rectangle-1.png')}
-          className="w-[200px] rounded-xl"
-        />
-        <View className="flex flex-col ml-2 w-[133px] items-start justify-end flex-[1_0_0]">
+      <View className="flex flex-row mt-4">
+        <View className="flex flex-row items-start justify-start w-3/5 pr-2 ">
           <Image
-            source={require('~/public/images/rectangle-2.png')}
-            className="flex-[1_0_0] self-stretch"
+            source={require('@/assets/images/20230707_194027.jpg')}
+            className="w-full h-[248px] rounded-xl bg-white shadow"
+            resizeMode="cover"
           />
+        </View>
+        <View className="flex flex-col w-2/5 pl-2">
           <Image
-            source={require('~/public/images/frame-1.png')}
-            className="flex-[1_0_0] self-stretch"
+            source={require('@/assets/images/20230707_210635.jpg')}
+            className="w-full h-[114px] mb-5 bg-white shadow rounded-xl"
+            resizeMode="cover"
           />
+          <ImageBackground
+            source={require('@/assets/images/20230707_184730.jpg')}
+            className="w-full h-[114px] flex bg-white shadow items-center justify-center overflow-hidden rounded-xl"
+            resizeMode="cover"
+          >
+            <View className="flex flex-row items-center justify-center w-full bg-tuatura/50 blur-[22px] h-11">
+              <Text className="text-xl text-white font-mont">+120</Text>
+            </View>
+          </ImageBackground>
         </View>
       </View>
     </View>
